caad64399bf5b8f864b7a13bef850f62dc7d0d47
José Carlos Vélez <josvelsei@gmail.com>
2025-01-15 14:31:21 +0100

    Update routes.ts

--- BESafePassGenerator/routes/routes.ts
+++ BESafePassGenerator/routes/routes.ts
@@ -1,10 +1,11 @@
 
 import { Router } from "express";
 import PasswordController from "../controller/PasswordController";
-import PasswordService from "../services/password.service";
+import { PasswordService } from "../core/services/password.service";
+import { PasswordRepository } from "../Repository/password.repository";
 
 const router = Router();
-const passwordService = new PasswordService();
+const passwordService = new PasswordService(new PasswordRepository());
 const passwordController = new PasswordController(passwordService);
 
 // Define las rutas y enlaza los métodos
@@ -14,7 +15,7 @@ router.post("/api/passwords", passwordController.createPassword.bind(passwordCon
 router.put("/api/passwords/:id", passwordController.updatePassword.bind(passwordController));
 router.delete("/api/passwords/:id", passwordController.deletePassword.bind(passwordController));
 
-router.get("/api", (req, res) => {
+router.get("", (req, res) => {
   res.send("API");
 });
 
